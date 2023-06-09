cb34a5a1b352bc8c59033b4247739913a4ede11d
bowstringhero <hyeonjun217.lee@gmail.com>
2023-06-09 10:18:25 +0900

    sideProject

---
+++ src/member/Admin.java
@@ -0,0 +1,17 @@
+package member;
+
+public class Admin extends Person {
+    private String id = "Admin";
+    private String password = "1234";
+
+    public Admin(String name, int phone) {
+        super(name, phone);
+    }
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
